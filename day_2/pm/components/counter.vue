2537095d2030e09a345c8471446aef5879c3ba90
Alex <alex@orion.sh>
2019-12-05 15:25:09 +0100

    plus ou moins

---
+++ day_2/pm/components/counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>Plus ou moins? De 0 à 1 000!</h2>
+    <p>{{ message }}</p>
+    <b-form-input type="number" v-model="guess" @keyup.enter="find"></b-form-input>
+    <button v-if="reset" @click="secret = Math.floor((Math.random() * 1000) + 1)">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      secret: Math.floor((Math.random() * 1000) + 1),
+      guess: 0,
+      message: '',
+      reset: false
+    }
+  },
+  methods: {
+    find () {
+      if (this.guess > this.secret) {
+        this.message = "C'est moins!"
+      } else if (this.guess < this.secret) {
+        this.message = "C'est plus!"
+      } else {
+        this.message = "Gagné!"
+        this.reset = true
+      }
+    }
+  }
+}
+</script>
---
+++ day_2/pm/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-container>
+    <counter></counter>
+  </b-container>
+</template>
+
+<script>
+import counter from '@/components/counter.vue'
+export default {
+  name: 'Homepage',
+  data () {
+    return {
+      title: 'Bonjour'
+    }
+  },
+  components: {
+    counter
+  }
+}
+</script>
