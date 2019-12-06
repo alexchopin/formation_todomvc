e0f7e9dc510d8bec8529c6f7c7d516148fed2179
Alex <alex@orion.sh>
2019-12-06 14:03:35 +0100

    formulaire

--- day_2/pm/layouts/default.vue
+++ day_2/pm/layouts/default.vue
@@ -2,6 +2,7 @@
   <div>
     <div>
       <nuxt-link to="/">Accueil</nuxt-link> | 
+      <nuxt-link to="/form">Formulaire</nuxt-link> | 
       <nuxt-link to="/login">Login</nuxt-link> | 
       <nuxt-link to="/users">Users</nuxt-link> | 
       <p v-if="$store.getters.isConnected">{{ $store.state.user.name }}</p>
---
+++ day_2/pm/pages/form.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <pre>{{ forms }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $axios }) {
+    const forms = await $axios.$get("/forms.json")
+    return {
+      forms
+    }
+  }
+}
+</script>
